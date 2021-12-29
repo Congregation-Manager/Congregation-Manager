52c7fdb5878dea8757562d6bfcd30fb8c7570812
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2021-12-29 15:28:16 +0100

    Add easy deploy recipe (#3)

---
+++ config/prod/deploy.php
@@ -0,0 +1,24 @@
+<?php
+
+use EasyCorp\Bundle\EasyDeployBundle\Configuration\DefaultConfiguration;
+use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
+
+return new class extends DefaultDeployer {
+    public function configure(): DefaultConfiguration
+    {
+        return $this->getConfigBuilder()
+            ->server('vagrant@congregation-manager.org')
+            ->deployDir('/var/www/congregation-manager')
+            ->repositoryUrl('git@github.com:lruozzi9/congregation-manager.git')
+            ->repositoryBranch('issue-3')
+            ->sharedFilesAndDirs(['.env.local', 'var/log']);
+    }
+
+    /**
+     * Needed for https://github.com/EasyCorp/easy-deploy-bundle/issues/78
+     */
+    public function beforePreparing(): void
+    {
+        $this->runRemote('cp {{ deploy_dir }}/repo/.env {{ project_dir }}/.env');
+    }
+};
