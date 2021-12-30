8a76647ceca84b1c0c6a17992cabbad31a96ba6b
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2021-12-30 11:43:05 +0100

    Add deploy for JS files (#9)

--- config/prod/deploy.php
+++ config/prod/deploy.php
@@ -1,6 +1,7 @@
 <?php
 
 use EasyCorp\Bundle\EasyDeployBundle\Configuration\DefaultConfiguration;
+use EasyCorp\Bundle\EasyDeployBundle\Configuration\Option;
 use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
 
 return new class extends DefaultDeployer {
@@ -20,5 +21,24 @@ return new class extends DefaultDeployer {
     public function beforePreparing(): void
     {
         $this->runRemote('cp {{ deploy_dir }}/repo/.env {{ project_dir }}/.env');
+    }
+
+    public function beforePublishing(): void
+    {
+        $this->log('Remote yarn');
+        $this->runRemote('yarn install');
+        $this->runRemote('yarn run build');
+
+        $this->log('Remote composer dump env prod');
+        $this->runRemote(sprintf('%s dump-env prod', $this->getConfig(Option::remoteComposerBinaryPath)));
+
+        $this->log('Remote migration');
+        $this->runRemote('{{ console_bin }} doctrine:migration:migrate --no-interaction --allow-no-migration');
+
+        $this->log('Remote cache clear');
+        $this->runRemote('{{ console_bin }} cache:clear');
+
+        $this->log('Remote cache warmpup');
+        $this->runRemote('{{ console_bin }} cache:warmup');
     }
 };
