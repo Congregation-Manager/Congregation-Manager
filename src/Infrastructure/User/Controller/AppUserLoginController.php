234426530a7135718dd07a54f99da5f1e429e5d1
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-01-06 15:11:53 +0100

    Add login and logout for app firewall (#6)

---
+++ src/Infrastructure/User/Controller/AppUserLoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Infrastructure\User\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+final class AppUserLoginController extends AbstractController
+{
+    public function __construct(
+        private AuthenticationUtils $authenticationUtils
+    ) {
+    }
+
+    public function index(): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_homepage');
+        }
+
+        $error = $this->authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $this->authenticationUtils->getLastUsername();
+
+        return $this->render('app/login/index.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+}
