b3f6708b0821330627a4ac390d16bf63b3264497
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-03-03 23:29:51 +0100

    Hash password in CreateAppUser (#19)

--- src/Application/User/CreateAppUser.php
+++ src/Application/User/CreateAppUser.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace CongregationManager\Application\User;
 
 use CongregationManager\Domain\Congregation\Model\BrotherInterface;
+use CongregationManager\Domain\User\Hasher\UserPasswordHasherInterface;
 use CongregationManager\Domain\User\Model\AppUser;
 use CongregationManager\Domain\User\Model\AppUserInterface;
 use CongregationManager\Domain\User\Repository\AppUserRepositoryInterface;
@@ -12,17 +13,21 @@ use CongregationManager\Domain\User\Repository\AppUserRepositoryInterface;
 final class CreateAppUser
 {
     public function __construct(
-        private AppUserRepositoryInterface $appUserRepository
+        private AppUserRepositoryInterface $appUserRepository,
+        private UserPasswordHasherInterface $userPasswordHasher
     ) {
     }
 
     public function create(
         BrotherInterface $brother,
         string $email,
-        ?string $password = null,
+        ?string $plainPassword = null,
         ?string $localeCode = null
     ): AppUserInterface {
-        $appUser = new AppUser($brother, $email, $password, $localeCode);
+        $appUser = new AppUser($brother, $email, null, $localeCode);
+        if ($plainPassword !== null) {
+            $appUser->setPassword($this->userPasswordHasher->hashPasswordForUser($plainPassword, $appUser));
+        }
         $this->appUserRepository->add($appUser);
 
         return $appUser;
--- tests/Unit/Application/User/CreateAppUserTest.php
+++ tests/Unit/Application/User/CreateAppUserTest.php
@@ -7,20 +7,32 @@ namespace CongregationManager\Tests\Unit\Application\User;
 use CongregationManager\Application\User\CreateAppUser;
 use CongregationManager\Domain\Congregation\Model\Brother;
 use CongregationManager\Domain\Congregation\Model\Congregation;
+use CongregationManager\Domain\User\Hasher\UserPasswordHasherInterface;
+use CongregationManager\Domain\User\Model\UserInterface;
 use CongregationManager\Domain\User\Repository\AppUserRepositoryInterface;
 use CongregationManager\Tests\Repository\AppUserRepository;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\ObjectProphecy;
 
 class CreateAppUserTest extends TestCase
 {
+    use ProphecyTrait;
+
     private AppUserRepositoryInterface $appUserRepository;
 
     private CreateAppUser $createAppUser;
 
+    /** @var ObjectProphecy|UserPasswordHasherInterface  */
+    private $userPasswordHasher;
+
     protected function setUp(): void
     {
         $this->appUserRepository = new AppUserRepository();
-        $this->createAppUser = new CreateAppUser($this->appUserRepository);
+        $this->userPasswordHasher = $this->prophesize(UserPasswordHasherInterface::class);
+        $this->userPasswordHasher->hashPasswordForUser('p455w0rd', Argument::type(UserInterface::class))->willReturn('3r34fQDEWw3d');
+        $this->createAppUser = new CreateAppUser($this->appUserRepository, $this->userPasswordHasher->reveal());
     }
 
     public function test_that_it_creates_a_new_app_user(): void
@@ -30,8 +42,30 @@ class CreateAppUserTest extends TestCase
 
         $this->assertEquals($brother, $appUser->getBrother());
         $this->assertEquals('info@email.com', $appUser->getEmail());
-        $this->assertEquals('p455w0rd', $appUser->getPassword());
+        $this->assertEquals('3r34fQDEWw3d', $appUser->getPassword());
         $this->assertEquals('it_IT', $appUser->getLocaleCode());
         $this->assertEquals($this->appUserRepository->findAll()->first(), $appUser);
+    }
+
+    public function test_that_it_creates_a_new_app_user_without_password_if_not_specified(): void
+    {
+        $brother = new Brother('John', 'Ritz', new Congregation('Carrollton'));
+        $adminUser = $this->createAppUser->create($brother, 'info@email.com', null, 'it_IT');
+
+        $this->assertEquals('info@email.com', $adminUser->getEmail());
+        $this->assertNull($adminUser->getPassword());
+        $this->assertEquals('it_IT', $adminUser->getLocaleCode());
+        $this->assertEquals($this->appUserRepository->findAll()->first(), $adminUser);
+    }
+
+    public function test_that_it_creates_a_new_app_user_without_locale_if_not_specified(): void
+    {
+        $brother = new Brother('John', 'Ritz', new Congregation('Carrollton'));
+        $adminUser = $this->createAppUser->create($brother, 'info@email.com', 'p455w0rd', null);
+
+        $this->assertEquals('info@email.com', $adminUser->getEmail());
+        $this->assertEquals('3r34fQDEWw3d', $adminUser->getPassword());
+        $this->assertNull($adminUser->getLocaleCode());
+        $this->assertEquals($this->appUserRepository->findAll()->first(), $adminUser);
     }
 }
