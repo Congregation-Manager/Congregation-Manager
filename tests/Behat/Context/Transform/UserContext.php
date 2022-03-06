f469f7afee5645aeb11151956d1faac477ed20f4
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-03-06 17:05:30 +0100

    Fix Behat (#19)

--- tests/Behat/Context/Transform/UserContext.php
+++ tests/Behat/Context/Transform/UserContext.php
@@ -10,12 +10,29 @@ use CongregationManager\Infrastructure\User\Model\AdminUser;
 use CongregationManager\Infrastructure\User\Model\AppUser;
 use CongregationManager\Infrastructure\User\Model\AppUserInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use InvalidArgumentException;
 
 final class UserContext implements Context
 {
     public function __construct(
         private EntityManagerInterface $entityManager
     ) {
+    }
+
+    /**
+     * @Transform :appUser
+     * @Transform /^app user "([^"]+)"$/
+     */
+    public function getAppUserByEmail(string $email): AppUserInterface
+    {
+        /** @var AppUserInterface $appUser */
+        $appUserRepository = $this->entityManager->getRepository(AppUser::class);
+        $appUser = $appUserRepository->findOneBy(['email' => $email]);
+        if (null === $appUser) {
+            throw new InvalidArgumentException(sprintf('App user with email "%s" does not exist.', $email));
+        }
+
+        return $appUser;
     }
 
     /**
