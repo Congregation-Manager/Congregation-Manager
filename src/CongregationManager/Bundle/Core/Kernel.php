f96a7451df20ff0fe26e66e85b1a98db0d69ba15
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2023-01-06 17:19:35 +0100

    Fix kernel project dir (#253)

--- src/CongregationManager/Bundle/Core/Kernel.php
+++ src/CongregationManager/Bundle/Core/Kernel.php
@@ -6,8 +6,14 @@ namespace CongregationManager\Bundle\Core;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use function dirname;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getProjectDir(): string
+    {
+        return dirname(__DIR__, 4);
+    }
 }
