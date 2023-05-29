94117f3cce439649df0d158126479d1e1aeba18d
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2023-05-29 21:07:38 +0200

    Move assets in bundles (#253)

--- src/CongregationManager/Bundle/App/assets/app.js
+++ src/CongregationManager/Bundle/App/assets/app.js
@@ -1,12 +1,5 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/common.css';
+import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
@@ -14,3 +7,5 @@ import './bootstrap';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import './homepage';
