a0a14b360ab58309ecb1a5d3f64c50e04e2ccdf5
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2023-05-30 14:31:40 +0200

    Fix Psalm (#253)

--- src/CongregationManager/Bundle/Admin/config/config.php
+++ src/CongregationManager/Bundle/Admin/config/config.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-return static function (ContainerConfigurator $containerConfigurator) {
+return static function () {
 };
